7d40f2210f5b06ad7d54bd8af8b8423c6c8c71cb
Kate Simmons <kate.a.simmons@gmail.com>
2015-07-20 15:25:13 -0500

    ryu states implemented

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
-	$('.ryu').mouseleave(function() {
+	})
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
+	.mousedown(function() {
+		// play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// show hadouken and animate it
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-still').show();
+	});
 });
