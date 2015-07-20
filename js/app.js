b9a98f2f153f5dd91b6531376806bcd1746aab35
Kate Simmons <kate.a.simmons@gmail.com>
2015-07-20 15:42:03 -0500

    hadouken sound added

--- js/app.js
+++ js/app.js
@@ -8,14 +8,27 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// show hadouken and animate it
+		$('.hadouken').show().animate(
+			{'left': '1020px'}, 
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-still').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
